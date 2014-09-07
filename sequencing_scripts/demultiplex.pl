af52b02fed578cbddb616b9c6bd1c2945355c57d
bulksoil <edwards@ucdavis.edu>
2014-09-07 16:32:10 -0700

    Update comments, added usage statement

--- sequencing_scripts/demultiplex.pl
+++ sequencing_scripts/demultiplex.pl
@@ -20,7 +20,16 @@ use warnings;
 use Getopt::Std;
 
 my %opts = ();
-getopts('b:m:', \%opts); # b for barcode fastq and m for mapping file
+getopts('b:m:u', \%opts); # b for barcode fastq and m for mapping file
+
+my $usage = "Usage:
+demultiplex.pl -b barcodes.fq -m mapping_file.txt
+
+-b\tbarcode fastq file - a product of concat_fq.pl
+-m\tQIIME mapping file with the sample names in the first column and barcodes in the Second
+-u\tprint usage\n";
+
+die "$usage" if $opts{u};
 
 
 open MAP, "$opts{m}" or die "Cannot open the mapping file\n"; 	# Shut down and warn if no mapping file
@@ -29,27 +38,27 @@ open BAR, "$opts{b}" or die "Cannot open the barcode file\n"; 	# Shut down and w
 # Open the mapping file and create a hash that hash with barcode as key and the sample name as the value
 my %bcs = ();																	
 while (my $line = <MAP>) {													# Start while loop
-	chomp $line;																# Get rid of line break at the end of the line
+	chomp $line;															# Get rid of line break at the end of the line
 	next if $line =~ /^#/;													# Skip the line if it's commented out
 	my @fields = split(/\t/, $line);										# Split the line by tabs and save into array
-	my $bc = $fields[1];														# Second member of the array is the barcode
+	my $bc = $fields[1];													# Second member of the array is the barcode
 	my $samp = $fields[0];													# First member of the array is the sample name
 	$bcs{$bc} = $samp;														# Load everything into the hash
 }
 
-my $found = 0;																	# Create a counter for the amount of barcodes found
+my $found = 0;																# Create a counter for the amount of barcodes found
 
-while (my $header = <BAR>) {     										# Start looping through the barcode FQ file
-	chomp $header;																# Get rid of the line breaks
+while (my $header = <BAR>) {     											# Start looping through the barcode FQ file
+	chomp $header;															# Get rid of the line breaks
 	my $seq = <BAR>; chomp $seq;											# Save the sequence and remove LB
-	my $skip = <BAR>;															# Skip the next line
-	my $qual = <BAR>; chomp $qual;										# Save the quality into a scalar and remove LB
+	my $skip = <BAR>;														# Skip the next line
+	my $qual = <BAR>; chomp $qual;											# Save the quality into a scalar and remove LB
   
 	if (exists $bcs{$seq}) {												# See if the barcode exists in the previous hash
-		print "$header\t$bcs{$seq}\t$seq\n";							# If it does, print out the header and the sample id and the barcode sequence
-		$found++;																# Add to the amount of barcodes found
+		print "$header\t$bcs{$seq}\t$seq\n";								# If it does, print out the header and the sample id and the barcode sequence
+		$found++;															# Add to the amount of barcodes found
 	} else {
-		next;																		# If not found, go to the next line
+		next;																# If not found, go to the next line
 	}
-	warn "$found bcs found\n" if ($found % 10000 == 0);			# Update in real time how many barcodes are found
+	warn "$found bcs found\n" if ($found % 10000 == 0);						# Update in real time how many barcodes are found
 }
