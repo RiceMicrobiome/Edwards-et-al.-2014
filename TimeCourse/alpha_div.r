04e7a667f43e4350f93d4fa7f6f0d353ce42dbb0
bulksoil <edwards@ucdavis.edu>
2014-11-06 10:48:19 -0800

    Put more statistics in for alpha diversities in the acquisition experiment

--- TimeCourse/alpha_div.r
+++ TimeCourse/alpha_div.r
@@ -2,7 +2,7 @@ library(vegan)
 library(ggplot2)
 
 ## Load in the data
-load("~/RMB/TimeCourse/TC+GH/Data/tc_gh_data.rda")
+load("~/RMB/Publication/TimeCourse/TC+GH/Data/tc_gh_data.rda")
 otu.table <- tc.gh.data[[1]]
 map <- tc.gh.data[[2]]
 tax <- tc.gh.data[[3]]
@@ -17,7 +17,18 @@ row.names(dav.otu) <- paste("Otu", row.names(dav.otu), sep = "")
 dav.adiv <- cbind(dav.map, Shannon = diversity(t(dav.otu)))
 dav.adiv$Compartment <- gsub("Bulk_Soil", "Bulk Soil", dav.adiv$Compartment)
 dav.adiv$Compartment <- factor(dav.adiv$Compartment, levels = c("Bulk Soil", "Rhizosphere", "Rhizoplane", "Endosphere"))
+dav.adiv <- subset(dav.adiv, Days <= 13)
+summary(aov(exp(Shannon) ~ Compartment * Days, data = dav.adiv))
+summary(aov(exp(Shannon) ~ Days, data = subset(dav.adiv, Compartment == "Bulk Soil")))
+summary(aov(exp(Shannon) ~ Days, data = subset(dav.adiv, Compartment == "Rhizosphere")))
+summary(aov(exp(Shannon) ~ Days, data = subset(dav.adiv, Compartment == "Rhizoplane")))
+summary(aov(exp(Shannon) ~ Days, data = subset(dav.adiv, Compartment == "Endosphere")))
 
+kruskal.test(exp(Shannon) ~ Compartment * Days, data = dav.adiv)
+kruskal.test(exp(Shannon) ~ Days, data = subset(dav.adiv, Compartment == "Bulk Soil"))
+kruskal.test(exp(Shannon) ~ Days, data = subset(dav.adiv, Compartment == "Rhizosphere"))
+kruskal.test(exp(Shannon) ~ Days, data = subset(dav.adiv, Compartment == "Rhizoplane"))
+s
 ## Plot the results
 comp.cols <- c("#E41A1C", "#984EA3", "#4DAF4A", "#377EBA")
 ggplot(subset(dav.adiv, Days > 0), aes(x = "", y = exp(Shannon), fill = Compartment)) +
